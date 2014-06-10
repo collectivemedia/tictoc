40a8d4cf43623104d18a33d236c5cbf0ee4f9c15
Sergei Izrailev <sizrailev@collective.com>
2014-06-10 03:01:11 -0400

    tests for tic/toc functionality

---
+++ inst/tests/test-tictoc.R
@@ -0,0 +1,79 @@
+#-------------------------------------------------------------------------------
+#
+# Package tictoc 
+#
+# tests for tic/toc functionality
+# 
+# Sergei Izrailev, 2014
+#-------------------------------------------------------------------------------
+# Copyright 2011-2014 Collective, Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#-------------------------------------------------------------------------------
+
+context("Test tic/toc and related methods")
+
+#-------------------------------------------------------------------------------
+
+test_that("tic measures elapsed time in seconds", 
+{
+   x <- proc.time()["elapsed"]; y <- tic()
+   
+   expect_equal(as.logical(abs(x - y) < 0.01), TRUE)    
+   
+   tic(); Sys.sleep(2); tm <- toc(quiet = TRUE)
+   expect_equal(as.logical(abs(tm$toc - tm$tic - 2) < 0.1), TRUE)    
+})   
+
+#-------------------------------------------------------------------------------
+
+test_that("nested tic/tocs work",
+{
+   tic("outer")
+      Sys.sleep(1) 
+      tic("middle") 
+         Sys.sleep(2) 
+         tic("inner")
+            Sys.sleep(3) 
+         tm3 <- toc(quiet = TRUE)
+      tm2 <- toc(quiet = TRUE)
+   tm1 <- toc(quiet = TRUE)
+   
+   expect_equal(as.logical(abs(tm3$toc - tm3$tic - 3) < 0.1), TRUE)    
+   expect_equal(as.logical(abs(tm2$toc - tm2$tic - 5) < 0.1), TRUE)    
+   expect_equal(as.logical(abs(tm1$toc - tm1$tic - 6) < 0.1), TRUE)
+   expect_equal(tm1$msg, "outer")
+   expect_equal(tm2$msg, "middle")
+   expect_equal(tm3$msg, "inner")
+})
+
+#-------------------------------------------------------------------------------
+
+test_that("tic.clear works", 
+{
+   tic()
+   Sys.sleep(1) 
+   tic() 
+   Sys.sleep(2) 
+   tic()
+   Sys.sleep(3) 
+   tm3 <- toc(quiet = TRUE)
+   
+   # we still have two tic() calls to unwind
+   tic.clear()
+   tm <- toc(quiet = TRUE)
+   expect_equal(is.null(tm), TRUE)
+})
+
+#-------------------------------------------------------------------------------
+
